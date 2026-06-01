f5983466121faea1d305f97d3679aba04f30c7ac
jt99test <jordi.tyrrell@gmail.com>
2026-06-01 07:30:40 -0300

    Redirect legacy domain hosts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,69 @@
 import type { NextConfig } from "next";
 import { legacyRedirects } from "./lib/legacy-redirects";
 
+const legacyDomainRedirects = [
+  {
+    source: "/",
+    has: [{ type: "host", value: "youryogapills.org" }],
+    destination: "https://saritashakti.com/es",
+    statusCode: 301,
+  },
+  {
+    source: "/",
+    has: [{ type: "host", value: "www.youryogapills.org" }],
+    destination: "https://saritashakti.com/es",
+    statusCode: 301,
+  },
+  {
+    source: "/",
+    has: [{ type: "host", value: "en.youryogapills.org" }],
+    destination: "https://saritashakti.com/en",
+    statusCode: 301,
+  },
+  {
+    source: "/",
+    has: [{ type: "host", value: "es.youryogapills.org" }],
+    destination: "https://saritashakti.com/es",
+    statusCode: 301,
+  },
+  {
+    source: "/",
+    has: [{ type: "host", value: "it.youryogapills.org" }],
+    destination: "https://saritashakti.com/it",
+    statusCode: 301,
+  },
+  {
+    source: "/:path*",
+    has: [{ type: "host", value: "youryogapills.org" }],
+    destination: "https://saritashakti.com/:path*",
+    statusCode: 301,
+  },
+  {
+    source: "/:path*",
+    has: [{ type: "host", value: "www.youryogapills.org" }],
+    destination: "https://saritashakti.com/:path*",
+    statusCode: 301,
+  },
+  {
+    source: "/:path*",
+    has: [{ type: "host", value: "en.youryogapills.org" }],
+    destination: "https://saritashakti.com/:path*",
+    statusCode: 301,
+  },
+  {
+    source: "/:path*",
+    has: [{ type: "host", value: "es.youryogapills.org" }],
+    destination: "https://saritashakti.com/es/:path*",
+    statusCode: 301,
+  },
+  {
+    source: "/:path*",
+    has: [{ type: "host", value: "it.youryogapills.org" }],
+    destination: "https://saritashakti.com/it/:path*",
+    statusCode: 301,
+  },
+] satisfies Awaited<ReturnType<NonNullable<NextConfig["redirects"]>>>;
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -15,7 +78,7 @@ const nextConfig: NextConfig = {
     ],
   },
   async redirects() {
-    return legacyRedirects;
+    return [...legacyDomainRedirects, ...legacyRedirects];
   },
 };
 
